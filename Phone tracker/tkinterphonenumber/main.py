c49429725d3033497d6fb53b418ae7a9501de152
owinoweb <owinoweb@gmail.com>
2022-02-28 13:56:21 +0300

    trying to create the function

--- Phone tracker/tkinterphonenumber/main.py
+++ Phone tracker/tkinterphonenumber/main.py
@@ -10,20 +10,24 @@ root = Tk()
 root.title('Geolocater')
 root.geometry("800x500")
 
-def change():
-    number = input("please input  number that we can use: ")
+def change(number):
+    number = input("number")
     ch_number = phonenumbers.parse(number, "CH")
     print(geocoder.description_for_number(ch_number,  'en'))
     service_provider = phonenumbers.parse(number, "RO")
     print(carrier.name_for_number(service_provider, "en"))
     my_number.delete(0, END)
-    
+    asweroFImportsnve  = f"Name is of the service provider is {ch_number} and the number is {service_provider}."
+    return asweroFImportsnve
 
 my_number = Entry(root, font = ('Roboto', 28))
 my_number.pack(pady=20)
 
 mybutton = Button(root, text = 'input any number', command=change )
 mybutton.pack(pady=20)
+
+mylabel = Label(root , text =' change.asweroFImportsnve' )
+mylabel.pack(pady=20)
 
 
 root.mainloop()
