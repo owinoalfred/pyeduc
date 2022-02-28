8cf7c26f31adf48a9df58e6d3a7182a774db293b
owinoweb <owinoweb@gmail.com>
2022-02-28 13:37:32 +0300

    iniitialising the button

--- Phone tracker/tkinterphonenumber/main.py
+++ Phone tracker/tkinterphonenumber/main.py
@@ -1,13 +1,29 @@
-from logging import root
-from tkinter import *
-from main import *
 
-print(number)
+from tkinter import *
+import phonenumbers 
+from phonenumbers import geocoder
+from phonenumbers import carrier
+
+
 
 root = Tk()
 root.title('Geolocater')
 root.geometry("800x500")
 
+def change():
+    number = input("please input  number that we can use: ")
+    ch_number = phonenumbers.parse(number, "CH")
+    print(geocoder.description_for_number(ch_number,  'en'))
+    service_provider = phonenumbers.parse(number, "RO")
+    print(carrier.name_for_number(service_provider, "en"))
+    my_number.delete(0, END)
+    
+
+my_number = Entry(root, font = ('Roboto', 28))
+my_number.pack(pady=20)
+
+mybutton = Button(root, text = 'input any number', command=change )
+mybutton.pack(pady=20)
 
 
 root.mainloop()
